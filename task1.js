58c3c386588e0113e2af922757d88ffeaaa40b9d
mahadev <somshettymahadev@gmail.com>
2017-06-13 12:32:17 +0530

    new code

--- task1.js
+++ task1.js
@@ -10,88 +10,91 @@
 
 
 var taskButton = document.getElementById("addbutton");
-var addtaskButton = document.getElementById("addtask");
-
 
 var incompletedTask = document.getElementById("incompletedtask");
 var completedTask = document.getElementById("completedtask");
 
 
-var lists, spanText, editText, delButton, checktask, saveButton, taskContent;
-
-
-
-
 
 function addtask() {
-    taskContent = document.getElementsByName('addtask')[0].value;
-    lists = document.createElement("li");
-    spanText = document.createElement("span");
-    editText = document.createElement("input");
-    checktask = document.createElement("input");
-    saveButton = document.createElement("button");
-    delButton = document.createElement("button");
+
+    var taskContent = document.getElementsByName('addtask')[0].value;
+    var lists = document.createElement("li");
+    var spanText = document.createElement("span");
+    var editText = document.createElement("input");
+    var checktask = document.createElement("input");
+    var saveButton = document.createElement("button");
+    var delButton = document.createElement("button");
 
     editText.setAttribute("type", "text");
-    //editText.setAttribute("id", "edit");
+    editText.setAttribute("value", "");
     checktask.setAttribute("type", "checkbox");
 
-    saveButton.innerText = "edit";
+    saveButton.innerText = "Edit";
     delButton.innerText = "Delete";
 
     lists.appendChild(checktask);
     lists.appendChild(spanText);
     lists.appendChild(editText);
-    editText.style.display = "none";
+
     lists.appendChild(saveButton);
     lists.appendChild(delButton);
-
-    if (typeof (Storage) !== "undefined") {
-        console.log("local storage supported");
-       incompletedTask.appendChild(lists);
-
-    } else {
-        console.log("local storage not supported")
-    }
-
     if (taskContent !== "") {
         incompletedTask.appendChild(lists);
     }
+
+
+
+    // if (typeof (Storage) !== "undefined") {
+    //     console.log("local storage supported");
+    //     incompletedTask.appendChild(lists);
+
+    //     var todolists= [];
+    //     localStorage.setItem('todolists', JSON.stringify(todolists));
+
+    // } else {
+    //     console.log("local storage not supported")
+    // }
+
 
     spanText.innerText = taskContent;
     saveButton.onclick = editnsave;
     checktask.onchange = moveto;
     delButton.onclick = deletelist;
 
+    function editnsave() {
+
+
+        var editLi = lists.querySelector("input[type=text]");
+        console.log(editLi.value);
+
+
+        if (lists.className != "editmode") {
+            editLi.value = spanText.innerText;
+            lists.className = "editmode";
+            saveButton.innerText = "Save";
+
+        } else if (lists.className == "editmode") {
+            spanText.innerText = editLi.value;
+            saveButton.innerText = "Edit";
+            lists.className = "";
+
+
+        }
+    }
+
+    document.getElementsByName('addtask')[0].value = "";
 
 }
 taskButton.onclick = addtask;
 
-addtaskButton.onkeydown = function (enterkey) {
+document.onkeydown = function (enterkey) {
     if (enterkey.keyCode == 13) {
         addtask();
+        editnsave();
     }
 }
 
-function editnsave() {
-
-    if (saveButton.innerText == "edit") {
-
-        spanText.style.display = "none";
-        editText.style.display = "inline";
-        editText.value = spanText.innerText;
-        saveButton.innerText = "save";
-
-
-    } else if (saveButton.innerText == "save") {
-
-        spanText.style.display = "inline";
-        editText.style.display = "none";
-        spanText.innerText = editText.value;
-        saveButton.innerText = "edit";
-
-    }
-}
 
 
 
--- tasks.js
+++ tasks.js
@@ -14,22 +14,17 @@ var addtaskButton = document.getElementById("addtask");
 var incompletedTask = document.getElementById("incompletedtask");
 var completedTask = document.getElementById("completedtask");
 
-var lists, editText, spanText, taskContent, saveButton, delButton;
 var obj = {
 
     addtask: function () {
 
         var checktask = document.createElement("input");
-        lists = document.createElement("li");
-        spanText = document.createElement("span");
-        editText = document.createElement("input");
+        var lists = document.createElement("li");
+        var spanText = document.createElement("span");
+        var editText = document.createElement("input");
 
-        saveButton = document.createElement("button");
-        delButton = document.createElement("button");
-
-
-
-        //var final = lists
+        var saveButton = document.createElement("button");
+        var delButton = document.createElement("button");
 
         editText.setAttribute("type", "text");
         editText.setAttribute("id", "edit");
@@ -39,44 +34,67 @@ var obj = {
         saveButton.innerText = "edit";
         delButton.innerText = "Delete";
 
-        incompletedTask.appendChild(lists);
         lists.appendChild(checktask);
         lists.appendChild(spanText);
-
+        lists.appendChild(editText);
         lists.appendChild(saveButton);
         lists.appendChild(delButton);
 
 
-        taskContent = document.getElementsByName('addtask')[0].value;
+        var taskContent = document.getElementsByName('addtask')[0].value;
 
-        spanText.innerHTML = taskContent;
+        spanText.innerText = taskContent;
         var final = document.getElementsByTagName("li");
-        
-        var arrlist = JSON.parse(localStorage.getItem("arrlist"));
+
         var arrlist = [];
 
         arrlist.push(final[0]);
-        
-        localStorage.setItem("arrlist", JSON.stringify(arrlist));
 
-        
+        localStorage.setItem('todoList', JSON.stringify(arrlist));
+
+
         incompletedTask.appendChild(arrlist[0]);
-        var arrlist = JSON.parse(localStorage.getItem("arrlist"));
 
-        
-        
+
         delButton.onclick = deletetask;
 
-        var inputFiled = document.getElementsByName('addtask')[0].value = "";
+
 
         saveButton.onclick = editnsave;
 
+        function editnsave() {
+
+
+
+            var editLi = lists.querySelector("input[type=text]");
+            console.log(editLi.value);
+
+
+            if (lists.className != "editmode") {
+                editLi.value = spanText.innerText;
+                lists.className = "editmode";
+
+            } else if (lists.className == "editmode") {
+                spanText.innerText = editLi.value;
+
+                lists.className = "";
+            }
+
+
+        }
+
+        var tasktext = document.getElementsByName('addtask')[0];
+        tasktext.value = " ";
+
+
     }
+
+
 }
 
 taskButton.onclick = obj.addtask;
 
-addtaskButton.onkeydown = function (enterkey) {
+onkeydown = function (enterkey) {
     if (enterkey.keyCode == 13) {
         obj.addtask();
     }
@@ -84,24 +102,5 @@ addtaskButton.onkeydown = function (enterkey) {
 
 function deletetask() {
     this.parentNode.remove();
-
-}
-
-function editnsave() {
-
-    if (saveButton.innerText == "save") {
-
-
-        var editedtext = document.getElementById("edit").value;
-
-        spanText.innerText = editedtext;
-
-        saveButton.innerText = "edit";
-    } else if (saveButton.innerText == "edit") {
-        var editval = spanText.appendChild(editText);
-        editval.value = taskContent;
-        saveButton.innerText = "save";
-
-    }
 
 }
